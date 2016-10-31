5d363dde597a018aa974afcbdbc3b36ca6835503
Yury Nechaev <nechaev.main@gmail.com>
2016-10-31 12:59:12 +0100

    Added YNImageAsync framework for test
    Fixed issue with Haneke assert failure.

---
+++ Pods/Target Support Files/YNImageAsync/YNImageAsync-umbrella.h
@@ -0,0 +1,9 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "YNImageAsync.h"
+
+FOUNDATION_EXPORT double YNImageAsyncVersionNumber;
+FOUNDATION_EXPORT const unsigned char YNImageAsyncVersionString[];
+
